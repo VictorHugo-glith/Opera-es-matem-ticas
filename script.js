284d9da706f3fa3b2e8c8369aadb05917968baf4
I1HSA <victor.vh1702@gmail.com>
2026-03-23 10:18:54 -0300

    subtracaov3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-     let numUm = Number(prompt("Me diga um número:"));
-        let numDois = Number(prompt("Agora me fale outro número:"));
-        let resultado = numUm - numDois;
+     let numUm = Number(prompt("Me diga um número:")); //criar váriavel para saber o primeiro valor da subtração;
+        let numDois = Number(prompt("Agora me fale outro número:")); //criar uma segunda váriavel para sabero segundo número;
+        let resultado = numUm - numDois; //criar váriavel para realizar a operação matemática;
 
-        alert("O resultado desse número é " + resultado + ".");
+        alert("O resultado desse número é " + resultado + "."); //alertar o resultado da operação.
