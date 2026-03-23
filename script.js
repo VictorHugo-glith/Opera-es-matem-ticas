9f0ce34dd077527e20eaafd2e2a14b3d7e1c9ea1
lucaskauan913@gmail.com <lucaskauan913@gmail.com>
2026-03-23 11:37:12 -0300

    correção

---
+++ script.js
@@ -0,0 +1,5 @@
+let numUm = Number(prompt("digite o primeiro número"));
+let numDois = Number(prompt("digite o segundo nome"));
+let resultadoMultiplicação = numUm * numDois;
+
+alert ("o resultado da multiplicação é igual a: " + resultadoMultiplicação );
