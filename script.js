eacd557b19d28dde76dd690c4109731a46fa934e
unknown <pedrohenriquebarbosamendes7@gmail.com>
2026-03-23 11:18:43 -0300

    cleanup

---
+++ script.js
@@ -0,0 +1,8 @@
+let numUm = Number(prompt("Insira o primeiro número:"));
+ let numDois = Number(prompt("Insira o segundo número:"));
+        //let:declarar variável
+        //Number:Transforma a varíavel em número
+let soma = (numUm+numDois);
+alert(`A soma de ${numUm} e ${numDois} é ${soma} `);
+        //Alert:jogar mensagem ou o resultado na tela do computador
+        //toFixed:Arredonda o número de acordo com o número de casa que vc quiser
