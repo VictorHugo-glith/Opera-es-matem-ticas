d0ab3604255366fda0ee328ec41b172312ebf43d
I1HSA <victor.vh1702@gmail.com>
2026-03-23 10:16:43 -0300

    subtracaov2

---
+++ script.js
@@ -0,0 +1,5 @@
+     let numUm = Number(prompt("Me diga um número:"));
+        let numDois = Number(prompt("Agora me fale outro número:"));
+        let resultado = numUm - numDois;
+
+        alert("O resultado desse número é " + resultado + ".");
