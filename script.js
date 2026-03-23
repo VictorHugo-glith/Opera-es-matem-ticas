75435a28f47d14ca42ea4ad9a84cbce22213345a
I1HSA <leonardofribeiro6@gmail.com>
2026-03-23 10:54:47 -0300

    correção para a operação correta

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let num1 = Number(prompt("insira o primeiro número"));
 let num2 = Number(prompt("insira o segundo número"));
-let resultadoSubtracao = num1 - num2;
+let resultadoDivisao = num1 / num2;
 
-alert("o resultado de sua subtração é " + resultadoSubtracao + "!");
+alert("o resultado de sua divisão é " + resultadoDivisao + "!");
